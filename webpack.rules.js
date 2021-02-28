c38f80d4d92b1dca429cf0275128d0304635675a
alirezainjast <allalirezamail@gmail.com>
2021-02-28 18:56:32 +0330

    ready for implementing react

--- webpack.rules.js
+++ webpack.rules.js
@@ -14,6 +14,14 @@ module.exports = [
       },
     },
   },
+  {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+    },
+  },
+
   // Put your webpack loader rules in this array.  This is where you would put
   // your ts-loader configuration for instance:
   /**
