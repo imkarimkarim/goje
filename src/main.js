e42265c6ffbc304981995e2e48badedf304fc569
alirezainjast <allalirezamail@gmail.com>
2021-02-28 19:07:21 +0330

    still boilerplate...

--- src/main.js
+++ src/main.js
@@ -17,7 +17,9 @@ const createWindow = () => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  if (process.env.DEBUG) {
+    mainWindow.webContents.openDevTools();
+  }
 };
 
 // This method will be called when Electron has finished
