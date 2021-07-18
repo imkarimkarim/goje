88bad92cb33f2b2efe08f62d6ded9558bf8b8517
realamiro <amirart8.artwork@gmail.com>
2021-07-18 16:59:47 +0430

    add cancel button in NotFound

--- src/App/Components/NotFound.jsx
+++ src/App/Components/NotFound.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ErrorIcon from "@material-ui/icons/Error";
+import Button from "@material-ui/core/Button";
 import Nav from "./Nav.jsx";
 import "./NotFound.css";
 
@@ -10,6 +11,16 @@ export default function NotFound() {
       <div className="notFound">
         <ErrorIcon />
         <p className="hint">مشکلی پیش آمده است! با پشتیبانی تماس بگیرید.</p>
+        <Button variant="outlined">
+          <span
+            className="button-title"
+            onClick={() => {
+              window.history.back();
+            }}
+          >
+            انصراف
+          </span>
+        </Button>
       </div>
     </div>
   );
