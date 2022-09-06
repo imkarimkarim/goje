8bee8cc021e8a206f8bda6c5f15dd37ba49149d7
imkarimkarim <allalirezamail@gmail.com>
2022-09-06 16:30:46 +0430

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 12,
+  },
+  plugins: ["react"],
+  rules: { "react/prop-types": "off" },
 };
