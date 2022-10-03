f65e8baa4c61b74742b15ad3571d84517687d8d0
karim <allalirezamail@gmail.com>
2022-10-03 18:00:06 +0330

    Print Sum Product Owner Selected Cars

--- src/App/Pages/Reports/PrintSumProductOwnerSelectedCars.jsx
+++ src/App/Pages/Reports/PrintSumProductOwnerSelectedCars.jsx
@@ -1,13 +1,100 @@
 const { ipcRenderer } = require("electron");
 import React, { useRef, useEffect, useContext } from "react";
-import { useParams } from "react-router-dom";
+import { Redirect, useParams } from "react-router-dom";
+import Header from "../../Components/Header.jsx";
+import Footer from "../../Components/Footer.jsx";
 import { NotifContext } from "../../Contexts/NotifContext.jsx";
+import Expense from "../../Components/Expense.jsx";
+import { Grid } from "@material-ui/core";
+import html2pdf from "html2pdf.js";
+import JDate from "jalali-date";
+import { useState } from "react";
+import Loading from "../../Components/Loading.jsx";
+import ShowDate from "../../Components/ShowDate.jsx";
+
+function SaleSection({ cars, ownerName }) {
+  const [goBack, setGoBack] = useState(false);
+
+  let fullSum = 0;
+  for (let car of cars) {
+    fullSum += car.ownerEarnings;
+  }
+
+  useEffect(() => {
+    if (!goBack) {
+      const jdate = new JDate();
+      const fileName = `صورتحساب های ${ownerName} - ${jdate.format(
+        "DD MMMM YYYY"
+      )}.pdf`;
+      const options = {
+        jsPDF: { format: "a5" },
+        filename: fileName,
+        html2canvas: { scale: 1 },
+      };
+      html2pdf()
+        .set(options)
+        .from(document.body)
+        .save()
+        .then(() => {
+          setGoBack(true);
+        });
+    }
+  });
+
+  return (
+    <div>
+      {goBack ? (
+        <Redirect to="/welcome" />
+      ) : (
+        <div className="sale">
+          <Grid container spacing={1}>
+            <Grid className="saleInfo-table" item xs={9}>
+              <table>
+                <thead>
+                  <tr>
+                    <th>تاریخ ورود</th>
+                    <th>پلاک</th>
+                    <th>کد ماشین</th>
+                    <th>صافی</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {cars.map((car, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{<ShowDate timestamp={car.arrivalDate} />}</td>
+                        <td>{car.plaque}</td>
+                        <td>{car.customeId}</td>
+                        <td>{<Expense num={car.ownerEarnings} />}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </Grid>
+          </Grid>
+          <div className="owner-earning-car">
+            <h3>
+              <span>مجموع صافی ها</span>
+              <span> :</span>
+              <span>
+                <Expense num={fullSum} />
+              </span>
+            </h3>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default function PrintSumProductOwnerSelectedCars() {
   const { clearNotifs } = useContext(NotifContext);
   let { ownerName, selectedCars } = useParams();
   selectedCars = selectedCars.split(",");
   const init = useRef(true);
+
+  const [cars, setCars] = useState();
 
   const sumProductOwnerSelectedCars = (carIds) => {
     ipcRenderer.send("sumProductOwnerSelectedCars", carIds);
@@ -21,7 +108,7 @@ export default function PrintSumProductOwnerSelectedCars() {
     }
 
     ipcRenderer.on("sumProductOwnerSelectedCars", (event, cars) => {
-      console.log(cars);
+      setCars(cars.reverse());
     });
 
     // clean up
@@ -29,12 +116,36 @@ export default function PrintSumProductOwnerSelectedCars() {
       ipcRenderer.removeAllListeners("sumProductOwnerSelectedCars");
     };
   });
+  const currentDate = new Date(new JDate()._d).getTime();
 
-  return (
+  return cars && cars.length >= 2 ? (
     <div>
-      {ownerName}
-      <br />
-      {selectedCars}
+      <div className="printCar">
+        <Header />
+
+        <div className="info">
+          <p className="safiTitle">
+            <span>صورتحساب</span>
+            <span> </span>
+            <span>
+              <h3>{ownerName}</h3>
+            </span>
+          </p>
+        </div>
+        <div>
+          <span>تاریخ گزارش</span>
+          <span> :</span>
+          <span>
+            <ShowDate timestamp={currentDate} />
+          </span>
+        </div>
+
+        <br />
+        <SaleSection cars={cars} ownerName={ownerName} />
+        <Footer />
+      </div>
     </div>
+  ) : (
+    <Loading />
   );
 }
