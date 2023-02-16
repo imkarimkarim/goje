325343e9f2a9125b4dbe994361c4be4540e029e5
imkarimkarim <imkarimkarim@pm.me>
2023-02-16 20:42:57 +0330

    update SearchResultItem

--- src/App/Components/Report/SearchResultItem.jsx
+++ src/App/Components/Report/SearchResultItem.jsx
@@ -7,50 +7,62 @@ import Divider from "@material-ui/core/Divider";
 import Checkbox from "@material-ui/core/Checkbox";
 import "./SearchResultItem.css";
 
-const SearchResultItem = React.memo(
-  ({ itemTitle, titleHint, customeId, onChecked, to }) => {
-    const [checked, setChecked] = useState(false);
+const SearchResultItem = React.memo(({ itemTitle, titleHint, customeId, onChecked, onClick, to, initChecked = false }) => {
+    const [checked, setChecked] = useState(initChecked);
     return onChecked ? (
-      <div className="SearchResultItem">
-        <Divider />
-        <ListItem button>
-          <Link to={to}>
-            <div>
-              {itemTitle} <span className="hint"> {titleHint}</span>
-            </div>
-          </Link>
+        <div className="SearchResultItem">
+            <Divider />
+            <ListItem
+                onClick={() => {
+                    onClick(customeId);
+                }}
+                button>
+                {to ? (
+                    <Link to={to}>
+                        <div>
+                            {itemTitle} <span className="hint"> {titleHint}</span>
+                        </div>
+                    </Link>
+                ) : (
+                    <div>
+                        {itemTitle} <span className="hint"> {titleHint}</span>
+                    </div>
+                )}
 
-          {onChecked ? (
-            <div>
-              <ListItemSecondaryAction>
-                <Checkbox
-                  edge="end"
-                  onChange={() => {
-                    setChecked(!checked);
-                    onChecked(customeId);
-                  }}
-                  checked={checked}
-                />
-              </ListItemSecondaryAction>
-            </div>
-          ) : (
-            <span></span>
-          )}
-        </ListItem>
-      </div>
+                {onChecked ? (
+                    <div>
+                        <ListItemSecondaryAction>
+                            <Checkbox
+                                edge="end"
+                                onChange={() => {
+                                    setChecked(!checked);
+                                    onChecked(customeId);
+                                }}
+                                checked={checked}
+                            />
+                        </ListItemSecondaryAction>
+                    </div>
+                ) : (
+                    <span></span>
+                )}
+            </ListItem>
+        </div>
     ) : (
-      <div className="SearchResultItem">
-        <Divider />
-        <Link to={to}>
-          <ListItem button>
-            <div>
-              {itemTitle} <span className="hint"> {titleHint}</span>
-            </div>
-          </ListItem>
-        </Link>
-      </div>
+        <div className="SearchResultItem">
+            <Divider />
+            <Link to={to}>
+                <ListItem
+                    onClick={() => {
+                        onClick(customeId);
+                    }}
+                    button>
+                    <div>
+                        {itemTitle} <span className="hint"> {titleHint}</span>
+                    </div>
+                </ListItem>
+            </Link>
+        </div>
     );
-  }
-);
+});
 
 export default SearchResultItem;
